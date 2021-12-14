ac9abc15c55f0c6e5654d932dc43ad50f389e9a6
Matheus Araújo <souzamatheus8174@gmail.com>
2021-12-14 19:35:00 -0300

    style: create Home style

---
+++ frontend/src/pages/Home/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  align-content: flex-start;
+
+  width: 100vw;
+  height: 100vh;
+`;
